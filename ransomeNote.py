5d4ed915676d58bd222a2adf37d476982e3fdb9b
MERVIN-dev254 <o.silverygold@gmail.com>
2022-09-15 17:59:32 +0300

    Ransome note - Leet code

---
+++ ransomeNote.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d = {}
+        e = {}
+        for i in ransomNote:
+            d[i] = ransomNote.count(i)
+        for i in magazine:
+            e[i] = magazine.count(i)
+            
+        rans_set = set(d)
+        maga_set = set(e)
+        
+        for k in d.keys():
+            if d[k] > e[k]:
+                return False
+            return True
+                    

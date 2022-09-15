f51f4b1cde020a0970afccab5fce43a9a3c9326f
MARVIN <81169099+MERVIN-dev254@users.noreply.github.com>
2022-09-15 18:04:38 +0300

    commenting out unnecesary code

--- ransomeNote.py
+++ ransomeNote.py
@@ -7,8 +7,8 @@ class Solution:
         for i in magazine:
             e[i] = magazine.count(i)
             
-        rans_set = set(d)
-        maga_set = set(e)
+#         rans_set = set(d)
+#         maga_set = set(e)
         
         for k in d.keys():
             if d[k] > e[k]:
